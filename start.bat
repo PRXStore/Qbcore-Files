29e4ae47c0f8e9cb36c1b66bcb8daa90a006b7cb
PRXStore <167324132+PRXStore@users.noreply.github.com>
2025-04-07 03:03:33 +0300

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,1 +1,1 @@
-D:\PR-Files-LOST\FXServer\FXServer.exe  +set sv_licenseKey licensekey +exec server.cfg +set onesync_population true +set onesync_enableBeyond true +set onesync legacy
+FXServer\FXServer.exe  +set sv_licenseKey licensekey +exec server.cfg +set onesync_population true +set onesync_enableBeyond true +set onesync legacy
